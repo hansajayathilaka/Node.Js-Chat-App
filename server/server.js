8360cd47cf1be661a4cf394123e523271ff3bab5
hansajayathilaka <hansajayathilaka@yahoo.com>
2019-12-09 17:08:45 +0530

    connect and disconnect listner.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const morgan = require('morgan');
 
 const publicPath = path.join(__dirname, '/../public');
-var port = process.env.PORT || 3000;
-var app = express();
+const port = process.env.PORT || 3000;
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
+app.use(morgan('dev'));
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', socket => {
+    console.log('A new user is Connected.');
+    socket.on('disconnect', () => {
+        console.log('A user is Disconnected.')
+    })
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
-})
+});
