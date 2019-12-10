0a9d178a9d36e6b27e6301d6f96b7f53d0f0daf1
hansajayathilaka <hansajayathilaka@yahoo.com>
2019-12-10 13:10:13 +0530

    Welcome Message Done.

--- server/server.js
+++ server/server.js
@@ -15,11 +15,39 @@ app.use(express.static(publicPath));
 
 io.on('connection', socket => {
     console.log('A new user is Connected.');
+
+    // Server Get the Message
+    socket.on('sendMsg', message => {
+
+        socket.emit('recMsg', {
+            from: "Admin",
+            text: "Welcome to the Chat Room...!",
+            createdAt: new Date().getTime()
+        });
+
+        socket.broadcast.emit('recMsg', {
+            from: "Admin",
+            text: "Someone is Joined to the Chat Room.",
+            createdAt: new Date().getTime()
+        });
+
+        socket.on('sendMsg', message => {
+            console.log(message);
+            io.emit('recMsg', {
+                from: message.from,
+                text: message.text,
+                createdAt: new Date().getTime()
+            });
+        });
+    });
+
+    // Disconnected
     socket.on('disconnect', () => {
         console.log('A user is Disconnected.')
     })
 });
 
+// Listen
 server.listen(port, () => {
     console.log(`Server is up on port ${port}.`);
 });
